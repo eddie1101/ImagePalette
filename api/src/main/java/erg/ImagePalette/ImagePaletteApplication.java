027c3a414f5f067fc2fba6e25a4b230dfc24a96b
Eddie <erg1101@outlook.com>
2022-10-05 17:14:03 -0400

    Initial Commit

---
+++ api/src/main/java/erg/ImagePalette/ImagePaletteApplication.java
@@ -0,0 +1,13 @@
+package erg.ImagePalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagePaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagePaletteApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/erg/ImagePalette/ImagePaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package erg.ImagePalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagePaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
