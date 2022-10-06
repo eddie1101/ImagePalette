a775685917b360d6493f135a2172581a9ca03e31
erg5995 <erg5995@rit.edu>
2022-10-06 15:36:32 -0400

    Some work on gathering colors from the image

--- api/src/main/java/erg/ImagePalette/ImageAnalyzer.java
+++ api/src/main/java/erg/ImagePalette/ImageAnalyzer.java
@@ -1,7 +1,24 @@
 package erg.ImagePalette;
 
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageAnalyzer {
 
-    
+    public ImageData getColorFrequency(String path) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("resources/" + path));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        if(img == null) return null;
+
+        return new ImageData(img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, 0));
+
+    }
 
 }
--- api/src/main/java/erg/ImagePalette/ImageData.java
+++ api/src/main/java/erg/ImagePalette/ImageData.java
@@ -1,7 +1,28 @@
 package erg.ImagePalette;
 
+import java.util.Arrays;
+
 public class ImageData {
 
-    
-    
+    int numPixels;
+    int[] r, g, b, a;
+    int[] colorSet;
+    int[] numColorPixels;
+
+    //Assume inputs are same length
+    public ImageData(int[] pixels) {
+        this.r = new int[pixels.length];
+        this.g = new int[pixels.length];
+        this.b = new int[pixels.length];
+        this.a = new int[pixels.length];
+        for(int i = 0; i < pixels.length; i++) {
+            r[i] = pixels[i] >> 24 & 0xFF;
+            g[i] = pixels[i] >> 16 & 0xFF;
+            b[i] = pixels[i] >> 8 & 0xFF;
+            a[i] = pixels[i] & 0xFF;
+
+            
+
+        }
+    }
 }
